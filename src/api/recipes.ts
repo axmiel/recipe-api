79e3a11dbaea8aed3a8af22142d7ee6caa214782
Arthur Smielewski <arthur.smielewski@gmail.com>
2025-04-29 22:30:00 +0100

    x (sorry adam)

--- src/api/recipes.ts
+++ src/api/recipes.ts
@@ -6,8 +6,25 @@ import { Recipe } from '../models/recipe';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const recipe = await Recipe.find({}).exec()
-  res.json(recipe)
+  const prepTime = req.query.prep_time
+  const query = prepTime ? {prep_time: {$lt: Number(prepTime)}} : {}
+  const recipes = await Recipe.find(query).exec()
+  res.json(recipes)
 });
+
+router.get('/:id', async (req, res) => {
+  const recipeId = req.params.id
+  const recipe = await Recipe.findById(recipeId).exec()
+  res.json(recipe);
+})
+
+//extend to get all recipes to get prep time < 30 mins (askjeeves when using express how to get query parameters)
+
+// router.get('?prep_time', async (req, res) => {
+//   const prepTime = req.query.
+//   const recipe = await Recipe.findById(recipeId).exec()
+//   res.json(recipe);
+// })
+
 
 export default router;
