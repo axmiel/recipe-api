e0936cd8f5be4309e0a09e86354c330c3354edb8
Arthur Smielewski <arthur.smielewski@gmail.com>
2025-05-11 16:51:30 +0100

    feat: DB schema update, refactored recipes function
    - updated the DB schema to include prep_time, category, cuisine parameters, removed filters parameter
    - refactored the recipes get function to allow multi select for categories, cuisines, and prep_time which should now work together and allow multi select

--- src/api/recipes.ts
+++ src/api/recipes.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import express from "express";
 
 import { Recipe } from "../models/recipe";
@@ -5,13 +6,28 @@ import { Recipe } from "../models/recipe";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const prepTime = req.query.prep_time;
-  const query = prepTime
-    ? { filters: {}, prep_time: { $lt: Number(prepTime) } }
-    : {};
+  const { prep_time, category, cuisine } = req.query;
+  
+  const query = {}
+
+  if (prep_time) {
+    query.prep_time = { $lt: Number(prep_time) }
+    console.log('query prep time')
+  }
+
+  if (category) {
+    const categoriesArray = category.split(',')
+    query.category = { $in: categoriesArray }
+  }
+
+  if (cuisine) {
+    const cuisinesArray = cuisine.split(',')
+    query.cuisine = { $in: cuisinesArray }
+  }
+
   const recipes = await Recipe.find(query).exec();
   res.json(recipes);
-});
+})
 
 router.get("/:id", async (req, res) => {
   const recipeId = req.params.id;
@@ -24,13 +40,5 @@ router.post("/", async (req, res) => {
   const newRecipeData = req.body;
   res.json(newRecipeData);
 });
-
-//extend to get all recipes to get prep time < 30 mins (askjeeves when using express how to get query parameters)
-
-// router.get('?prep_time', async (req, res) => {
-//   const prepTime = req.query.
-//   const recipe = await Recipe.findById(recipeId).exec()
-//   res.json(recipe);
-// })
 
 export default router;
--- src/models/recipe.js
+++ src/models/recipe.js
@@ -1,4 +1,13 @@
 import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({ name: String, image: String });
+const schema = new mongoose.Schema(
+    { 
+        name: String, 
+        image: String, 
+        prep_time: Number, 
+        category: { type: [String], default: [] }, 
+        cuisine: { type: [String], default: [] }
+    }
+);
+
 export const Recipe = mongoose.model('Recipes', schema);
